8db5440ecd0fca661892536e55c0fe60b742c332
mariussimion <ing.mariussimion@gmail.com>
2017-02-04 18:50:27 +0200

    M first project

---
+++ JAVAhomeworksSimionMarius/src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,19 @@
+/*
+ File of my first class writtent in Java
+ */
+package myfirstpackage;
+
+/**
+ *My fisrst Java class
+ * @author Marius
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("This is the opossum");
+    }
+    
+}
